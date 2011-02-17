c079ebeb1f097d9bfb65b2bca2353756ccf725ff
Rodolfo Spalenza <rodolfo.signorelli.spalenza@gmail.com>
2011-02-17 19:10:01 -0200

    Hello World - It's Works!

---
+++ application/controllers/helloworld.php
@@ -0,0 +1,13 @@
+<?php
+	class Helloworld extends CI_Controller {
+		function __construct() {
+			parent::__construct();
+		}
+		
+		function index() {
+			$this->load->view("helloworld");
+		}
+	}
+
+
+?>
---
+++ application/views/helloworld.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Hello World</title>
+	</head>
+	
+	<body>
+		<h1>It's Works!</h1>
+	</body>
+</html>
