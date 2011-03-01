2b699ca41186e31076f1d41e0bb2709bf5d4f565
rodolfospalenza <rodolfo.signorelli.spalenza@gmail.com>
2011-03-01 11:52:46 -0300

    Menu alligator administrador com dropdown click.

--- application/views/administracao/elementos/header.php
+++ application/views/administracao/elementos/header.php
@@ -13,8 +13,11 @@
 			?>
 			
 			<script type="text/javascript" 
-					src="externalfiles/js/jquery-1.5.1.min"
-				></script>	
+					src="<?php echo base_url(); ?>externalfiles/js/jquery-1.5.1.min.js">
+					</script>
+			<script type="text/javascript"
+					src="<?php echo base_url(); ?>externalfiles/js/menu.js">
+					</script>		
 	</head>
 
 	<body>
